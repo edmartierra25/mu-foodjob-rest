db46dabf1d6c57e86f40245f93853b4309c1d344
Christian Edmar Tierra <tierrch@MFCGD.com>
2024-03-15 10:19:34 +0800

    new login.js file

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/', function(req, res, next) {
+    const {username, password} = req.query;
+    if (username === 'admin' && password === 'admin') {
+        res.status(200).json({message: 'Login successful'});
+    } else {
+        res.status(401).json({message: 'Login failed'});
+    }
+});
+
 module.exports = router;
