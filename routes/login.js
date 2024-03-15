78cf42df8f33c1c24ee67cd31b2d1a833cbac900
Ronald Allan Cruz Patawaran <patawro@mfcgd.com>
2024-03-15 10:44:48 +0800

    added login

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    const {username, password} = req.query;
+    const {username, password} = req.body;
     if (username === 'admin' && password === 'admin') {
-        res.status(200).json({message: 'Login successful'});
+        res.status(200).json({
+            user_id: 1,
+            roles: "admin"
+        });
+    } else if (username === 'user' && password === 'user') {
+        res.status(200).json({
+            user_id: 2,
+            roles: "user"
+        });
     } else {
         res.status(401).json({message: 'Login failed'});
     }
